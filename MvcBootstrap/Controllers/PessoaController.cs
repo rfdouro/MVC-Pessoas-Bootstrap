688bd63a7dc5525e15694a6572a586692fd201e6
rfdouro <rfdouro@gmail.com>
2017-05-25 15:36:39 -0300

    01

---
+++ MvcBootstrap/Controllers/PessoaController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBootstrap.Models;
+using System.Data.Entity;
+
+namespace MvcBootstrap.Controllers
+{
+    public class PessoaController : Controller
+    {
+		PessoaContexto contexto = new PessoaContexto();
+
+        public ActionResult Index()
+        {
+			if (contexto != null)
+			{
+				List<Pessoa> lista = contexto.Pessoas.ToList();
+				return View(lista);
+			}	
+            return View ();
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View ();
+        }
+
+        public ActionResult Create()
+        {
+            return View ();
+        } 
+
+        [HttpPost]
+		public ActionResult Create(Pessoa p /*FormCollection collection*/)
+        {
+			//DbContextTransaction trans = null;
+            try {
+				//trans = contexto.Database.BeginTransaction();
+				contexto.Pessoas.Add(p);
+				contexto.SaveChanges();
+				//trans.Commit();
+                return RedirectToAction ("Index");
+            } catch {
+				//trans.Rollback();
+                return View ();
+            }
+        }
+        
+        public ActionResult Edit(int id)
+        {
+			try
+			{
+				Pessoa p = contexto.Pessoas.Find(id);
+				return View(p);
+			}
+			catch
+			{
+				return RedirectToAction("Index");
+			}
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Pessoa p/*int id, FormCollection collection*/)
+        {
+            try {
+				if (ModelState.IsValid)
+				{
+					contexto.Entry(p).State = EntityState.Modified;
+					contexto.SaveChanges();
+				}
+                return RedirectToAction ("Index");
+            } catch {
+                return View ();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+			try
+			{
+				Pessoa p = contexto.Pessoas.Find(id);
+				contexto.Pessoas.Remove(p);
+				contexto.SaveChanges();
+			}
+			catch
+			{
+
+			}
+			return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try {
+                return RedirectToAction ("Index");
+            } catch {
+                return View ();
+            }
+        }
+    }
+}
---
+++ MvcBootstrap/Models/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcBootstrap.Models
+{
+	public class Pessoa
+	{
+		[Key]
+		public long id { get; set;}
+		public String nome { get; set;}
+		public Pessoa()
+		{
+		}
+	}
+
+	public class PessoaContexto : DbContext
+	{
+		public PessoaContexto() : base()
+		{
+		}
+
+		public DbSet<Pessoa> Pessoas { get; set; }
+	}
+}
